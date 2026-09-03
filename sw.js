5390554c01c8964bb6d372b8ac628291048ed0dd
JIN RI <test1200012000@gmail.com>
2026-09-03 18:15:21 +0900

    fix: update PWA icon cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'gpja-meal-v1';
-const APP_SHELL = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './icon.svg'];
+const CACHE_NAME = 'gpja-meal-v2';
+const APP_SHELL = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest', './1788425827851.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
